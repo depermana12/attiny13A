8bf7ac4aa0a222365e785e1c845c471231cddc69
depermana12 <deddiapermana97@gmail.com>
2023-07-05 01:01:41 +0700

    Update 3 led chaser 2 blink interval main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,79 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 #include <avr/sleep.h>
-#include <avr/power.h>
-boolean buttonState = 0;         // current state of the button
-boolean lastButtonState = 0;     // previous state of the button
+#include <util/delay.h>
+
+volatile uint8_t buttonState = 0;         // current state of the button
+volatile uint8_t lastButtonState = 0;     // previous state of the button
+volatile uint8_t ledState = 0;            // current state of the LEDs
+
 void setup() {
    DDRB &= ~(1 << PB4);          // Set the pin PB4 as input
-   PORTB |= (1 << PB4);          //activate pull-up resistor for PB4
-   DDRB |= (1<<PB0);             // set PB1 as output(LED)
+   PORTB |= (1 << PB4);          // Activate pull-up resistor for PB4
+   DDRB |= (1 << PB0) | (1 << PB1) | (1 << PB2);  // Set PB0, PB1, PB2 as output (LEDs)
+
+   // Enable Pin Change Interrupt on PCINT4 (PB4)
+   PCICR |= (1 << PCIE0);
+   PCMSK0 |= (1 << PCINT4);
+
+   sei();  // Enable global interrupts
 }
+
 void sleep() {
-    GIMSK |= _BV(PCIE);                     // Enable Pin Change Interrupts
-    PCMSK |= _BV(PCINT4);                   // Use PB4 as interrupt pin
-    ADCSRA &= ~_BV(ADEN);                   // ADC off
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // replaces above statement
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();
+    sleep_cpu();
+    sleep_disable();
+}
 
-    sleep_enable();                         // Sets the Sleep Enable bit in the MCUCR Register (SE BIT)
-    sei();                                  // Enable interrupts
-    sleep_cpu();                            // sleep
+void toggleLedChaser500ms() {
+    static uint32_t timer = 0;
+    uint32_t interval = 500;  // Blink interval of 500ms
 
-    cli();                                  // Disable interrupts
-    PCMSK &= ~_BV(PCINT4);                  // Turn off PB4 as interrupt pin
-    sleep_disable();                        // Clear SE bit
-    //ADCSRA |= _BV(ADEN);                  // ADC on
-    sei();                                  // Enable interrupts
-    } // sleep
-ISR(PCINT0_vect) {
-    // This is called when the interrupt occurs, but I don't need to do anything in it
+    if ((millis() - timer) >= interval) {
+        timer = millis();
+        ledState = (ledState + 1) % 8;
+
+        PORTB = (PORTB & 0xF8) | ledState;
     }
-void loop() {
-   static unsigned long timer = 0;
-   unsigned long interval = 100;                   // check switch 20 times per second
-   if (millis() - timer >= interval) {
-      timer = millis();
-      buttonState = PINB & (1 << PB4);
-      if (buttonState != lastButtonState) {
-         if (buttonState == LOW) {
-            digitalWrite(PB0, !digitalRead(PB0)); // toggle the output
+}
+
+void toggleLedChaser1s() {
+    static uint32_t timer = 0;
+    uint32_t interval = 1000;  // Blink interval of 1s
+
+    if ((millis() - timer) >= interval) {
+        timer = millis();
+        ledState = (ledState + 1) % 8;
+
+        PORTB = (PORTB & 0xF8) | ledState;
+    }
+}
+
+ISR(PCINT0_vect) {
+    buttonState = PINB & (1 << PB4);
+
+    if (buttonState != lastButtonState) {
+        if (buttonState == LOW) {
+            ledState = 0;
         }
-  }
-      lastButtonState = buttonState;
-   }
-   sleep();
+    }
+   
+    lastButtonState = buttonState;
+}
+
+int main() {
+    setup();
+
+    while (1) {
+        if (ledState != 0) {
+            toggleLedChaser500ms();
+        } else {
+            toggleLedChaser1s();
+        }
+
+        sleep();
+    }
+
+    return 0;
 }
